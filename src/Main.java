cda7604ed41039ff13a0fc1b5b1e4f57cbcc4bac
Maksim <raymax.spb@gmail.com>
2024-03-15 17:37:41 +0300

    Домашнее задание 11 методы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
+    public static void calculateYear() {
+        int year = 1583;
+        if (year > 1584) {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(+year + " год вискосный");
+            } else {
+                System.out.println(+year + " год не вискосный");
+            }
+        } else {
+            System.out.println("Такого понятия как високосный год не существовало");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world! ");
+        System.out.println("Task 1");
+        calculateYear();
     }
 }
